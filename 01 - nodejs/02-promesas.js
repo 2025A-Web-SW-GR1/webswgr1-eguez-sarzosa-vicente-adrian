91bf29fba822d7674ef87876275052566db33744
Adrian Eguez <adrian.eguez@epn.edu.ec>
2025-05-27 17:02:37 -0500

    hasta promesa ejemplo

--- 01 - nodejs/02-promesas.js
+++ 01 - nodejs/02-promesas.js
@@ -1,1 +1,50 @@
 const fs = require('fs');
+
+function promesaEsPar(numero){
+    const miPrimerPromesa = new Promise(
+        (resolve,reject)=>{
+            if(numero % 2 === 0){
+                resolve(numero); // RETURN
+            }else{
+                reject('No es par!'); // THROW
+            }
+        }
+    )
+}
+function promesaElevarAlCuadrado(numero){
+    return new Promise(res=>res(numero * numero));
+}
+
+promesaEsPar(4) // async
+    .then( // continuo async
+        (respuestaEsPar)=>{
+            console.log('Es par', respuestaEsPar);
+            return promesaElevarAlCuadrado(
+                respuestaEsPar
+            );
+        }
+    )
+    .then( // try
+        (respuestaElevarCuadrado)=>{
+            console.log('Elevado: ', 
+                respuestaElevarCuadrado);
+        }
+    )
+    .catch(
+        (respuestaError)=>{
+            console.log('NO ES PAR', respuestaError);
+        }
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
